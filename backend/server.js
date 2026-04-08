fbb44c2d36e7d1e48e2ac047954730dd62371b8a
harshita <srivastavaharshita1704@gmail.com>
2026-04-08 20:16:28 +0530

    Connected to MongoDB atlas (resolve SRV issue using standard string

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+// Connect DB
+connectDB();
+
+//app_var = expree()
 const app = express();
 
+//middleware
 app.use(express.json());
 
-// Import routes
 const authRoutes = require("./routes/authRoutes");
-
-// Use routes
 app.use("/api/auth", authRoutes);
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => {    //router
     res.send("API running");
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {   //running 
+    console.log(`Server running on port ${PORT}`);
 });
