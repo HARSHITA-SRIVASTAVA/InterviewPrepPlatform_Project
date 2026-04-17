f18c851e4527bfd31f387bec29efee05184feda4
harshita <srivastavaharshita1704@gmail.com>
2026-04-17 17:38:25 +0530

    Added Password Hashing using bcrypt + salting

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,17 +1,16 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    // 1. Basic validation
     if (!name || !email || !password) {
       return res.status(400).json({
         message: "All fields are required",
       });
     }
 
-    // 2. Check if user already exists
     const userExists = await User.findOne({ email });
 
     if (userExists) {
@@ -20,15 +19,17 @@ const registerUser = async (req, res) => {
       });
     }
 
-    // 3. Create user : Use the model to save the new user into MongoDB
+    // 🔐 HASH PASSWORD
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Save hashed password
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
-    // 4. Response
-    //only send back the ID, Name, and Email—NEVER the password!
     res.status(201).json({
       message: "User registered successfully",
       user: {
