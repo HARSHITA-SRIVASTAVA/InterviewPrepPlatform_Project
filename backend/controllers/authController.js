c2322797394f61c2433829e4fc05e40a2abe490f
harshita <srivastavaharshita1704@gmail.com>
2026-04-17 13:16:35 +0530

    user registration API with validation and MongoDB integration

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,9 +1,48 @@
-const registerUser = (req, res) => {
-    console.log(req.body);
+const User = require("../models/User");
 
-    res.status(201).json({
-        message: "User registered (dummy)",
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // 1. Basic validation
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    // 2. Check if user already exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // 3. Create user : Use the model to save the new user into MongoDB
+    const user = await User.create({
+      name,
+      email,
+      password,
     });
+
+    // 4. Response
+    //only send back the ID, Name, and Email—NEVER the password!
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
 };
 
 module.exports = { registerUser };
