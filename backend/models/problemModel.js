806faf1043a339c90eedf54df02114e2db53c6c5
harshita <srivastavaharshita1704@gmail.com>
2026-04-19 12:39:12 +0530

    Problem schema with validation and timestamps

---
+++ backend/models/problemModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+//schema creation
+const problemSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Problem title is required"], //prevent empty
+      trim: true, //remove extra spaces
+    },
+
+    difficulty: {
+      type: String,
+      required: [true, "Difficulty is required"],
+      enum: {
+        values: ["Easy", "Medium", "Hard"],
+        message: "Difficulty must be Easy, Medium, or Hard",
+      },
+    },
+
+    tags: {
+      type: [String],
+      default: [],
+    },
+
+    link: {
+      type: String,
+      required: [true, "Problem link is required"],
+    },
+
+    platform: {
+      type: String,
+      default: "LeetCode",
+    },
+
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,  //auto add createdAt and updatedAt
+  }
+);
+
+//covert schema into model
+const Problem = mongoose.model("Problem", problemSchema);
+module.exports = Problem;
