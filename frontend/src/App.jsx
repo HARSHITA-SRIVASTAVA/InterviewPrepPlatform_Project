52b7aa2e54fe2e6e45ba0ddd606694e0be4ba96f
harshita <srivastavaharshita1704@gmail.com>
2026-04-23 18:42:48 +0530

    chore: remove incorrect root dependencies and isolate frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-3xl font-bold">
+        Smart Interview Prep Platform
+      </h1>
+    </div>
+  );
+}
+
+export default App;
