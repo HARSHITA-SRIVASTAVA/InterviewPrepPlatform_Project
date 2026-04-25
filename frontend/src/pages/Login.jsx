5216cbbe85192909dd1cafee20753168b0f74b43
harshita <srivastavaharshita1704@gmail.com>
2026-04-25 10:47:07 +0530

    feat: added basic validation to login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,62 +8,74 @@ const Login = () => {   //formData->curr val , setFormData->update data
     password: "",
   });
 
-  const { email, password } = formData;
+const { email, password } = formData;
 
 
-  //Handle input changes -> run whenever user type in input feilds
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,       //keep old value
-      [e.target.name]: e.target.value,   //get input name : user type 
-    }); 
-  };
+//Handle input changes -> run whenever user type in input feilds
+const handleChange = (e) => {
+  setFormData({
+    ...formData,       //keep old value
+    [e.target.name]: e.target.value,   //get input name : user type 
+  }); 
+};
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+  //handle submit
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  if (!email || !password) {
+    console.log("Please fill all fields");
+    return;
+  }
+
+  console.log("Form Data:", formData);
+};
+
+return (
+  <div className="min-h-screen flex items-center justify-center bg-gray-100">
       
-      <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md">
+    <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md">
         
-        <h2 className="text-2xl font-bold text-center mb-6">
-          Login
-        </h2>
+      <h2 className="text-2xl font-bold text-center mb-6">
+        Login
+      </h2>
 
-        <form>
+      <form onSubmit={handleSubmit}>
           
-          <div className="mb-4">
-            <label className="block mb-1 font-medium">Email</label>
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={handleChange}
-              placeholder="Enter your email"
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+        <div className="mb-4">
+          <label className="block mb-1 font-medium">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            placeholder="Enter your email"
+            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
 
-          <div className="mb-4">
-            <label className="block mb-1 font-medium">Password</label>
-            <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={handleChange}
-              placeholder="Enter your password"
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+        <div className="mb-4">
+          <label className="block mb-1 font-medium">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            placeholder="Enter your password"
+            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
 
-          <button
-            type="submit"
-            className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
-          >
-            Login
-          </button>
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
+        >
+          Login
+        </button>
 
-        </form>
-      </div>
+      </form>
     </div>
+  </div>
   );
 };
 
