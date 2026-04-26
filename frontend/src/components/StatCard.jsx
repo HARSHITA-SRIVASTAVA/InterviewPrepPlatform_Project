3cd80f614f67c472fb27b363c1879137f89804da
harshita <srivastavaharshita1704@gmail.com>
2026-04-26 18:48:42 +0530

    feat : add reusable statCard comp for dashboard UI

---
+++ frontend/src/components/StatCard.jsx
@@ -0,0 +1,10 @@
+const StatCard = ({ title, value }) => {
+  return (
+    <div className="bg-white shadow-md rounded-2xl p-6 w-60 text-center">
+      <h2 className="text-gray-500 text-sm">{title}</h2>
+      <p className="text-2xl font-bold mt-2">{value}</p>
+    </div>
+  );
+};
+
+export default StatCard;   //Reusable comp ->use anywhere 
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import API from "../api/axios";
-
+import StatCard from "../components/StatCard";
+ 
 const Dashboard = () => {
   const [stats, setStats] = useState(null);
 
@@ -25,23 +26,22 @@ const Dashboard = () => {
 
     fetchDashboard();
   }, []);
+    return (
+    <div className="p-6">
+        <h1 className="text-2xl font-bold mb-6">Dashboard</h1>
 
-  return (
-    <div>
-      <h1>Dashboard</h1>
-
-      {stats ? (
-        <div>
-          <p>Total: {stats.total}</p>
-          <p>Solved: {stats.solved}</p>
-          <p>Unsolved: {stats.unsolved}</p>
-          <p>Progress: {stats.progress}%</p>
+        {stats ? (
+        <div className="flex gap-6 flex-wrap">
+            <StatCard title="Total Problems" value={stats.total} />
+            <StatCard title="Solved" value={stats.solved} />
+            <StatCard title="Unsolved" value={stats.unsolved} />
+            <StatCard title="Progress (%)" value={stats.progress} />
         </div>
-      ) : (
+        ) : (
         <p>Loading...</p>
-      )}
+        )}
     </div>
-  );
+    );
 };
 
 export default Dashboard;
