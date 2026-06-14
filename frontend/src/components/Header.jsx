9c20d60dd3e7c3fd89b5ceafff5dfbeaf2d0911f
harshita <srivastavaharshita1704@gmail.com>
2026-06-14 14:47:51 +0530

    feat:Added logout button on header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,17 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 const Header = ({level}) => {
 
   const username="Coder";
+
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
   return (
     <div className="bg-white  px-8 py-4 flex items-center justify-between border-b border-gray-500">
 
@@ -35,6 +46,13 @@ const Header = ({level}) => {
             Level {level}
           </p>
         </div>
+
+        <button
+          onClick={handleLogout}
+          className="px-4 py-2 text-sm font-medium text-red-500 hover:bg-red-80 border rounded-2xl transition"
+        >
+          Logout
+        </button>
 
       </div>
     </div>
